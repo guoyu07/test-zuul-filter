167c34511533caf1494d516c03ac04cc2d81c43d
xujingfeng <250577914@qq.com>
2017-08-15 00:16:16 +0800

    first commit

---
+++ zuul-gateway/src/main/java/com/sinosoft/ZuulGatewayApp.java
@@ -0,0 +1,12 @@
+package com.sinosoft;
+
+/**
+ * Created by xujingfeng on 2017/8/14.
+ */
+public class ZuulGatewayApp {
+
+    public static void main(String[] args) {
+        SpringApplication
+    }
+
+}
